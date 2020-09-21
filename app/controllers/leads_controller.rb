be610450ab0371e9508ef6ae475730dc97b9c455
Sini Ninkovic <sini.sn.ninkovic@gmail.com>
2020-09-21 21:01:34 +0000

    safe params added

--- app/controllers/leads_controller.rb
+++ app/controllers/leads_controller.rb
@@ -1,8 +1,7 @@
 class LeadsController < ApplicationController
 
   def create
-
-    @lead = Lead.new
+    @lead = Lead.new lead_params
     if @lead.save
       redirect_to root_path, notice: "Saved"
     else
@@ -11,7 +10,6 @@ class LeadsController < ApplicationController
   end
 
 private
-
   def lead_params
     params.require(:lead).permit(:first_name, :last_name, :email)
   end
