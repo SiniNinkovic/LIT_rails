29994747373790dbc77f57114a4d0f2a907d05c9
Sini Ninkovic <sini.sn.ninkovic@gmail.com>
2020-09-21 20:26:27 +0000

    working subscription

---
+++ app/controllers/leads_controller.rb
@@ -0,0 +1,19 @@
+class LeadsController < ApplicationController
+
+  def create
+
+    @lead = Lead.new
+    if @lead.save
+      redirect_to root_path, notice: "Saved"
+    else
+      redirect_to root_path, alert: "Not Saved"
+    end
+  end
+
+private
+
+  def lead_params
+    params.require(:lead).permit(:first_name, :last_name, :email)
+  end
+
+end
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,7 @@
 class StaticPagesController < ApplicationController
 
   def home
+    @lead = Lead.new
   end
 
 end
